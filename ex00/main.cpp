b40b1a414fbb43d30b156e99a7d7ee13e51931c4
Leonardo Ottavi <leonardottavi02@gmail.com>
2025-01-14 12:33:43 +0100

    ex00 full

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:10:35 by lottavi           #+#    #+#             */
-/*   Updated: 2024/12/04 15:27:53 by lottavi          ###   ########.fr       */
+/*   Updated: 2025/01/14 12:32:44 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,23 @@ int	main()
 {
 	ClapTrap	claptrap("CL4P-TP");
 
+	int	damage1, damage2, repair1, repair2;
+
+	std::cout << "Enter damage for first attack: ";
+	std::cin >> damage1;
+	std::cout << "Enter repair amount after first attack: ";
+	std::cin >> repair1;
+	std::cout << "Enter damage for second attack: ";
+	std::cin >> damage2;
+	std::cout << "Enter repair amount after second attack: ";
+	std::cin >> repair2;
+
 	claptrap.attack("target1");
-	claptrap.takeDamage(5);
-	claptrap.beRepaired(3);
+	claptrap.takeDamage(damage1);
+	claptrap.beRepaired(repair1);
 	claptrap.attack("target2");
-	claptrap.takeDamage(10);
-	claptrap.beRepaired(5);
+	claptrap.takeDamage(damage2);
+	claptrap.beRepaired(repair2);
 
 	return 0;
 }
